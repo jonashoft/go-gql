917ceab12a1ca0ae9e7fe852d64fdd145ab0a3fc
Simon Bundgaard-Egeberg <seg@it-minds.dk>
2024-02-23 08:14:40 +0100

    🔨 chore: remove unused authMiddleware and import statements

--- server.go
+++ server.go
@@ -1,74 +1,18 @@
 package main
 
 import (
-	"fmt"
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/go-chi/chi/v5"
-	"github.com/golang-jwt/jwt/v5"
 	"graphql-go/auth"
 	"graphql-go/graph"
 	"graphql-go/persistence"
 	"log"
 	"net/http"
 	"os"
-	"strings"
-
-	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
 )
 
 const defaultPort = "8080"
-
-func authMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Host == "localhost:"+defaultPort {
-			next.ServeHTTP(w, r)
-			return
-		}
-
-		// Check for JWT in Authorization header
-		authHeader := r.Header.Get("Authorization")
-		if authHeader == "" {
-			http.Error(w, "Authorization header required", http.StatusUnauthorized)
-			return
-		}
-
-		// The header should be in the format `Bearer <token>`
-		parts := strings.Split(authHeader, " ")
-		if len(parts) != 2 || parts[0] != "Bearer" {
-			http.Error(w, "Invalid Authorization header format", http.StatusUnauthorized)
-			return
-		}
-
-		// Validate the JWT
-		tokenString := parts[1]
-		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-			// Make sure the token method is what we expect
-			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-				return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-			}
-
-			// Return the secret key (this should be more secure in production)
-			// Replace "your_secret_key" with your actual secret key
-			return []byte(os.Getenv("JWT_SECRET")), nil
-		})
-
-		if err != nil {
-			http.Error(w, "Invalid token", http.StatusUnauthorized)
-			return
-		}
-
-		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-			// You can access the token claims here if needed
-			fmt.Println(claims)
-		} else {
-			http.Error(w, "Invalid token", http.StatusUnauthorized)
-			return
-		}
-
-		// If the JWT is valid, proceed with the request
-		next.ServeHTTP(w, r)
-	})
-}
 
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -106,7 +50,6 @@ func main() {
 
 	// Use the Chi router methods to handle routes
 	router.Use(corsMiddleware)
-	router.Use(authMiddleware)
 	router.Use(auth.Middleware(gorm))
 	router.Handle("/query", srv)
 
